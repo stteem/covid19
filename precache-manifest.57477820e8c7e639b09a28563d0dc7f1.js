3f2078a5dd0c8da060784010afd76e77c7745b4d
Uwem Uke <slyart147@gmail.com>
2020-04-13 12:23:18 +0100

    Updates

---
+++ precache-manifest.57477820e8c7e639b09a28563d0dc7f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e3d15b63c469356e0b32a85111548cc",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "8593d5a99bcace815947",
+    "url": "/covid19/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "bcb20b9f7a1be14c8a13",
+    "url": "/covid19/static/css/main.3813a9e7.chunk.css"
+  },
+  {
+    "revision": "8593d5a99bcace815947",
+    "url": "/covid19/static/js/2.cf146d2d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/covid19/static/js/2.cf146d2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcb20b9f7a1be14c8a13",
+    "url": "/covid19/static/js/main.77167869.chunk.js"
+  },
+  {
+    "revision": "f7905e6c51f0df2a7dd5",
+    "url": "/covid19/static/js/runtime-main.667de02d.js"
+  }
+]);
