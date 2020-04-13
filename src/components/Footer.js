43a203370fa5e68d08ebf624e111cd1150c5f678
Uwem Uke <slyart147@gmail.com>
2020-04-13 16:48:56 +0100

    service worker

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,11 +8,10 @@ class Footer extends Component {
 			<div>
 				<h5>References</h5>
 				<ul>
-				<li>19.7 years is the AVG age in Africa - <a href="www.worldometers.info">worldometers.info</a></li>
-				<li>AVG daily income is ~$5 for 85% of Africans - <a href="www.blogs.worldbank.org">blogs.worldbank.org</a></li>
+				<li>19.7 years is the AVG age in Africa - <a href="https://www.worldometers.info/world-population/africa-population/">worldometers.info</a></li>
+				<li>AVG daily income is ~$5 for 85% of Africans - <a href="https://blogs.worldbank.org/opendata/85-africans-live-less-550-day">blogs.worldbank.org</a></li>
 				<li>COVID-19 infections doubles every 3 days. Expect 50x (at least 10x) reported cases to be actual
-					number of infected people - Jason S. Warner</li>
-				<li>Several stats from Harvard Medical School / Massachusetts General Hospital </li>
+					number of infected people - <a href="https://medium.com/@Jason_Scott_Warner/the-sober-math-everyone-must-understand-about-the-pandemic-2b0145881993">Jason S. Warner</a></li>
 				</ul>
 			</div>
 		);
